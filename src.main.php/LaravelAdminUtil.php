f82ca417089f2831ac94179362904c6e06a91d81
tanchengjin <18865477815@163.com>
2021-04-26 11:46:25 +0800

    fix laravel admin util

--- src.main.php/LaravelAdminUtil.php
+++ src.main.php/LaravelAdminUtil.php
@@ -7,8 +7,8 @@ class LaravelAdminUtil
     public static function getSwitchState($off = '关闭', $on = '开启', $onColor = 'success', $offColor = 'danger', $onValue = 1, $offValue = 0)
     {
         return [
-            'on' => ['value' => $offValue, 'text' => $on, 'color' => $onColor],
-            'off' => ['value' => $onValue, 'text' => $off, 'color' => $offColor],
+            'on' => ['value' => $onValue, 'text' => $on, 'color' => $onColor],
+            'off' => ['value' => $offValue, 'text' => $off, 'color' => $offColor],
         ];
     }
 }
